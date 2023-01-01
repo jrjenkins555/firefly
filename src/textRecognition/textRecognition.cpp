470f19b943475c9b765f58753e7d00f863053e67
alexj07 <alexjimenez9910@gmail.com>
2023-01-01 13:41:45 -0600

    IN PROGRESS: trying to get tesseract to import properly

--- src/textRecognition/textRecognition.cpp
+++ src/textRecognition/textRecognition.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include "textRecognition.h"
+#include "../../library/leptonica/1.82.0_1/include/leptonica/allheaders.h"
+#include "../../library/tesseract/5.2.0/include/tesseract/baseapi.h"
 
 char* TextRecognitionService::getText(imageCapture image){
     std::cout << "This is the text recognition service" << std::endl;
+
+    tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
+    api ->Init(NULL, "eng" );
 }
 
